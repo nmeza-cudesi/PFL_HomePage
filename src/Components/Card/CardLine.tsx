bb2ab59ba31c4b65cfe62484cfbbd5f7a16af9fb
nmeza-cudesi <76980692+nmeza-cudesi@users.noreply.github.com>
2022-09-03 20:28:32 -0400

    asd

---
+++ src/Components/Card/CardLine.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+
+export const CardLine = () => {
+    return (
+        <Box>
+        </Box>
+    )
+}
---
+++ src/Components/ImageBack/ImageBack.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+
+export const ImageBack = () => {
+    return (
+        <Box>
+        </Box>
+    )
+}
